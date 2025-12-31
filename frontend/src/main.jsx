d59d3c4873ff920397e7b7adcf04c0388b6b3825
mohamedirsath07 <mohdirsath072005@gmail.com>
2025-12-31 16:13:55 +0530

    FIxed Google OAuth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 import App from './App.jsx'
 
 // Replace with your actual Google Client ID
-const GOOGLE_CLIENT_ID = "832490195150-uvb0tj2qs9gsj6rkquhtgs8j6qug36kh.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "832490195150-igro2ni31839e7jbi0scc7cbh6u684p7.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
